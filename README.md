ee74575cd0dbc20afc4a5c42fa10ca5977da41cc
Ivan Sizov <vsizov63@gmail.com>
2022-09-02 04:15:13 +0300

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Проект: Шоссе, ТТ и Грэвел
+
+
+### Используемые технологии
+- Pug
+- Sass (SCSS)
+- Webpack
+- JS (OOP)
+- PostCSS
+- Babel
+
+
+### [Ссылка на Github Pages](https://frrenzy.github.io/gravel)
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: '[name].[contenthash:8].js'
+  },
+  entry: {
+    index: './src/index.pug',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  plugins: [
+    new PugPlugin({
+      extractCss: {
+        filename: '[name].[contenthash:8].css',
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader,
+        options: {
+          method: 'render',
+        },
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              import: false,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
